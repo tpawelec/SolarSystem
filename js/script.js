38f77aafed980a1aad373b2f36a4c48ce7c600bc
Tomasz Pawelec <tpawelec@outlook.com>
2019-04-16 11:30:12 +0200

    Added Saturn Rings. Needed to override RingGeometry constructor from ThreeJS R102 to R67 for texture to display properly. I bet there is a way to modify R102 contructor but I don't know it yet.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,74 @@
+THREE.RingGeometry = function ( innerRadius, outerRadius, thetaSegments, phiSegments, thetaStart, thetaLength ) {
+
+	THREE.Geometry.call( this );
+
+	innerRadius = innerRadius || 0;
+	outerRadius = outerRadius || 50;
+
+	thetaStart = thetaStart !== undefined ? thetaStart : 0;
+	thetaLength = thetaLength !== undefined ? thetaLength : Math.PI * 2;
+
+	thetaSegments = thetaSegments !== undefined ? Math.max( 3, thetaSegments ) : 8;
+	phiSegments = phiSegments !== undefined ? Math.max( 3, phiSegments ) : 8;
+
+	var i, o, uvs = [], radius = innerRadius, radiusStep = ( ( outerRadius - innerRadius ) / phiSegments );
+
+	for ( i = 0; i <= phiSegments; i ++ ) { // concentric circles inside ring
+
+		for ( o = 0; o <= thetaSegments; o ++ ) { // number of segments per circle
+
+			var vertex = new THREE.Vector3();
+			var segment = thetaStart + o / thetaSegments * thetaLength;
+
+			vertex.x = radius * Math.cos( segment );
+			vertex.y = radius * Math.sin( segment );
+
+			this.vertices.push( vertex );
+			//uvs.push( new THREE.Vector2( ( vertex.x / outerRadius+1)/2, ( vertex.y / outerRadius + 1 ) / 2 ) );
+			uvs.push( new THREE.Vector2( i/thetaSegments, o/ phiSegments ) );
+
+
+		}
+
+		radius += radiusStep;
+
+	}
+
+	var n = new THREE.Vector3( 0, 0, 1 );
+
+	for ( i = 0; i < phiSegments; i ++ ) { // concentric circles inside ring
+
+		var thetaSegment = i * thetaSegments;
+
+		for ( o = 0; o <= thetaSegments; o ++ ) { // number of segments per circle
+
+			var segment = o + thetaSegment;
+
+			var v1 = segment + i;
+			var v2 = segment + thetaSegments + i;
+			var v3 = segment + thetaSegments + 1 + i;
+
+			this.faces.push( new THREE.Face3( v1, v2, v3, [ n.clone(), n.clone(), n.clone() ] ) );
+			this.faceVertexUvs[ 0 ].push( [ uvs[ v1 ].clone(), uvs[ v2 ].clone(), uvs[ v3 ].clone() ]);
+
+			v1 = segment + i;
+			v2 = segment + thetaSegments + 1 + i;
+			v3 = segment + 1 + i;
+
+			this.faces.push( new THREE.Face3( v1, v2, v3, [ n.clone(), n.clone(), n.clone() ] ) );
+			this.faceVertexUvs[ 0 ].push( [ uvs[ v1 ].clone(), uvs[ v2 ].clone(), uvs[ v3 ].clone() ]);
+
+		}
+	}
+
+	this.computeFaceNormals();
+
+	this.boundingSphere = new THREE.Sphere( new THREE.Vector3(), radius );
+
+};
+
+THREE.RingGeometry.prototype = Object.create( THREE.Geometry.prototype );
+
 let loader, scene, camera, renderer;
 let gui, cam, focusGui;
 let PlanetAxis;
@@ -9,6 +80,7 @@ let textures = {};
 let geometries = {};
 let planetSpheres = {};
 let thetas = {};
+let SaturnRingsGeom, SaturnRings, RingsMaterial;
 
 
 let controls;
@@ -106,7 +178,6 @@ function init() {
     });
     focusOpt.open();
 
-    console.table(focusOptions)
     /*
         ORBIT CONTROLS
     */
@@ -159,6 +230,15 @@ function init() {
         thetas[planet] = 0;
         scene.add(planetSpheres[planet]);
     }
+
+    SaturnRingsGeom = new THREE.RingGeometry(scaleSize(Planets.Saturn.diameter) + 5, scaleSize(Planets.Saturn.diameter) + 35, 62, 62);
+    RingsMaterial = new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('img/saturnring.jpg'), side: THREE.DoubleSide } );
+    
+    SaturnRings = new THREE.Mesh(SaturnRingsGeom, RingsMaterial);
+    SaturnRings.position.set(0,0,0);
+    SaturnRings.rotation.z = degToRad(Planets.Saturn.obliquityToOrbit);
+    SaturnRings.rotation.x = Math.PI / 2;
+    planetSpheres.Saturn.add(SaturnRings);
     currentObj = SunSphere;
 }
 
